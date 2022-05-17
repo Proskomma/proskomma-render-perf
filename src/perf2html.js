06f89298666fb3d45918e876428874fb4ca7b2d4
Mark Howe <mvahowe@gmail.com>
2022-05-17 11:36:38 +0200

    HTML tweaks du jour

--- src/perf2html.js
+++ src/perf2html.js
@@ -13,8 +13,8 @@ const cvObjectHtml = bi => `<span class="${bi.type}">${bi.number}</span>`;
 const previewText = g => g.subType in previewTextFormats ? previewTextFormats[g.subType](g) : g.previewText;
 
 const previewTextFormats = {
-    xref: () => "†",
-    footnote: () => "*",
+    // xref: () => "†",
+    // footnote: () => "*",
     title: g => `TITLE: ${g.previewText}${g.nBlocks > 1 ? ` (${g.nBlocks} paras)`: ''}`,
     introduction: g => `INTRODUCTION: ${g.previewText.split(' ').slice(0, 25).join(' ')} [...] (${g.nBlocks} para${g.nBlocks > 0 ? 's' : ''})`,
 };
@@ -24,17 +24,19 @@ const perf2html = perf => {
     const [docSetId, docSetOb] = Object.entries(perf.docSets)[0];
     const [bookCode, documentOb] = Object.entries(docSetOb.documents)[0];
     const [sequenceId, sequenceOb] = Object.entries(documentOb.sequences).filter(s => s[1].selected)[0];
-    return `<div id="sequence" data-docSetId="${docSetId}" data-bookCode="${bookCode}" data-sequenceId="${sequenceId}">
+    return `<div id="document" data-docSetId="${docSetId}" data-bookCode="${bookCode}">
+<div id="sequence" data-sequenceId="${sequenceId}">
   <div id="headers">
 ${
         Object.entries(documentOb.headers).map(h =>
-            `    <div class="header" id="header_${h[0]}">${h[1]}</div>`
+            `    <div class="header ${h[0]}" id="${h[0]}">${h[1]}</div>`
         ).join('\n')
     }
   </div>
   <div id="content">
 ${sequenceOb.blocks.map(b => b.type === 'graft' ? bgHtml(b) : blockHtml(b)).join('\n')}
   </div>
+</div>
 </div>`
 }
 
