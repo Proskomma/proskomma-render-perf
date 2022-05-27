4c11cd289bb3edda2b57a0929812b8a926944d6d
Mark Howe <mvahowe@gmail.com>
2022-05-27 22:35:18 +0200

    Add test from USFM with rem

--- test/code/smoke.cjs
+++ test/code/smoke.cjs
@@ -7,7 +7,7 @@ import {doRender} from '../../src';
 const testGroup = 'Smoke';
 
 test(
-    `Hello (${testGroup})`,
+    `From Succinct (${testGroup})`,
     async function (t) {
         try {
             t.plan(1);
@@ -33,6 +33,40 @@ test(
             );
             t.equal(config2.validationErrors, null);
             // console.log(JSON.stringify(config2.output.docSets["eBible/fra_fraLSG"].documents["JON"].sequences, null, 2));
+            // console.log(config2.validationErrors);
+        } catch (err) {
+            console.log(err);
+        }
+    },
+);
+
+test(
+    `From USFM with REM (${testGroup})`,
+    async function (t) {
+        try {
+            t.plan(1);
+            const pk = new UWProskomma();
+            const usfm = fse.readFileSync(path.resolve(path.join(__dirname, '..', 'test_data', 'bcs-hi_irv.rev.usfm'))).toString();
+            pk.importDocuments({org: 'bcs', lang: 'hi', abbr: 'irv'}, 'usfm', [usfm]);
+            const config = {
+                selectedSequenceId: null,
+                allSequences: false,
+                output: {
+                    docSets: {},
+                }
+            };
+            const query = '{docSets { id documents {id bookCode: header(id:"bookCode")} } }';
+            const gqlResult = pk.gqlQuerySync(query);
+            const docSetId = gqlResult.data.docSets[0].id;
+            const documentId = gqlResult.data.docSets[0].documents.filter(d => d.bookCode === 'REV')[0].id;
+            const config2 = await doRender(
+                pk,
+                config,
+                [docSetId],
+                [documentId],
+            );
+            t.equal(config2.validationErrors, null);
+            // console.log(JSON.stringify(config2.output.docSets["bcs/hi_irv"].documents["REV"].sequences, null, 2));
             // console.log(config2.validationErrors);
         } catch (err) {
             console.log(err);
