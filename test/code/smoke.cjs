172464738ae6fd6d62c94fb68e68c543017353bb
Mark Howe <mvahowe@gmail.com>
2022-06-16 12:44:14 +0200

    Working property tags test

--- test/code/smoke.cjs
+++ test/code/smoke.cjs
@@ -38,7 +38,7 @@ test(
                 [docSetId],
                 [documentId],
             );
-            // const resultDocument = Object.values(config2.documents)[0];
+            const resultDocument = Object.values(config2.documents)[0];
             // console.log(JSON.stringify(config2.validations, null, 2));
             // console.log(JSON.stringify(resultDocument, null, 2));
             // console.log(JSON.stringify(resultDocument.sequences[resultDocument.main_sequence_id], null, 2));
@@ -68,6 +68,8 @@ test(
             const gqlResult = pk.gqlQuerySync(query);
             const docSetId = gqlResult.data.docSets[0].id;
             const documentId = gqlResult.data.docSets[0].documents.filter(d => d.bookCode === 'REV')[0].id;
+            pk.gqlQuerySync(`mutation { addDocSetTags(docSetId: "${docSetId}", tags: ["production", "title:IRV"]) }`);
+            pk.gqlQuerySync(`mutation { addDocumentTags(docSetId: "${docSetId}", documentId: "${documentId}", tags: ["checked", "isAligned:false"]) }`);
             const config2 = await doRender(
                 pk,
                 config,
@@ -75,8 +77,7 @@ test(
                 [documentId],
             );
             t.deepEqual(invalidDocs(config2.validations), {});
-            // console.log(JSON.stringify(config2.output.docSets["bcs/hi_irv"].documents["REV"].sequences, null, 2));
-            // console.log(config2.validationErrors);
+            // console.log(JSON.stringify(config2.documents[documentId], null, 2));
         } catch (err) {
             console.log(err);
         }
